40cb07d2f2e58f473474367f6a2e5f82592fc5f8
Ramon Cuni <mail@ramoncuni.com>
2015-10-23 10:10:58 +0200

    calcular vidre compost escrit, no comprovat

--- script.js
+++ script.js
@@ -243,14 +243,278 @@ function montaPreu() {
     case 'clst1674':
       preu += calculaDesconte(50.9);
       milimetros += 4;
+      break;
+    case 'cslt1676':
+      preu += calculaDesconte(53.94);
+      milimetros += 6;
+      break;
+    case 'sun4':
+      preu += calculaDesconte(43.82);
+      milimetros += 4;
+      break;
+    case 'sun6':
+      preu += calculaDesconte(56.64);
+      milimetros += 6;
+      break;
+    case 'ref6':
+      preu += calculaDesconte(34.16);
+      milimetros += 6;
+      break;
+    case 'clst6150':
+      preu += calculaDesconte(73.49);
+      milimetros += 6;
+      break;
+    case 'clst6120':
+      preu += calculaDesconte(79.62);
+      milimetros += 6;
+      break;
+    case 'stop5':
+      preu += calculaDesconte(35.29);
+      milimetros += 5;
+      break;
+    case 'plan4':
+      preu += calculaDesconte(22.36);
+      milimetros += 4;
+      break;
+    case 'plan6':
+      preu += calculaDesconte(32.14);
+      milimetros += 6;
+      break;
+    case 'planfn4':
+      preu += calculaDesconte(11.9);
+      milimetros += 4;
+      break;
+    case 'planfn6':
+      preu += calculaDesconte(18.7);
+      milimetros += 6;
+      break;
+    case 'kglas':
+      preu += calculaDesconte(20.6);
+      milimetros += 6;
+      break;
     }
-    //cercar es preu de substitució de sa camara
-
+    //cercam es preu de substitució de sa camara
+    var camara = document.getElementById('camara').value;
+    switch (camara) {
+    case 'cam8':
+      preu += calculaDesconte(0.3);
+      break;
+    case 'cam10':
+      preu += calculaDesconte(0.6);
+      break;
+    case 'cam12':
+      preu += calculaDesconte(0.9);
+      break;
+    case 'cam15':
+      preu += calculaDesconte(1.5);
+      break;
+    case 'cam18':
+      preu += calculaDesconte(1.8);
+      break;
+    case 'cam20':
+      preu += calculaDesconte(3);
+      break;
+    case 'cam24':
+      preu += calculaDesconte(3.5);
+      break;
+    }
     //cercam es preu de substitució des vidre interior
+    switch (vidreexterior) {
+    case 'float5':
+      preu += calculaDesconte(6.23);
+      milimetros += 5;
+      break;
+    case 'float6':
+      preu += calculaDesconte(8.5);
+      milimetros += 6;
+      break;
+    case 'float8':
+      preu += calculaDesconte(22.67);
+      milimetros += 8;
+      break;
+    case 'float10':
+      preu += calculaDesconte(39.67);
+      milimetros += 10;
+      break;
+    case 'parsol4':
+      preu += calculaDesconte(17.75);
+      milimetros += 4;
+      break;
+    case 'carglas4':
+      preu += calculaDesconte(8.32);
+      milimetros += 4;
+      break;
+    case 'carglas6':
+      preu += calculaDesconte(11.65);
+      milimetros += 6;
+      break;
+    case '33inc':
+      preu += calculaDesconte(25);
+      milimetros += 6;
+      break;
+    case '44inc':
+      preu += calculaDesconte(31);
+      milimetros += 8;
+      break;
+    case '55inc':
+      preu += calculaDesconte(38);
+      milimetros += 10;
+      break;
+    case '66inc':
+      preu += calculaDesconte(44);
+      milimetros += 10;
+      break;
+    case '33mate':
+      preu += calculaDesconte(41.15);
+      milimetros += 6;
+      break;
+    case '44mate':
+      preu += calculaDesconte(47.15);
+      milimetros += 8;
+      break;
+    case '55mate':
+      preu += calculaDesconte(54.15);
+      milimetros += 10;
+      break;
+    case '66mate':
+      preu += calculaDesconte(60.15);
+      milimetros += 12;
+      break;
+    case '33sil':
+      preu += calculaDesconte(41.3);
+      milimetros += 6;
+      break;
+    case '44sil':
+      preu += calculaDesconte(45.9);
+      milimetros += 8;
+      break;
+    case '55sil':
+      preu += calculaDesconte(49.74);
+      milimetros += 10;
+      break;
+    case '66sil':
+      preu += calculaDesconte(52.7);
+      milimetros += 12;
+      break;
+    case '44stb120':
+      preu += calculaDesconte(79.97);
+      milimetros += 8;
+      break;
+    case '66stb120':
+      preu += calculaDesconte(102);
+      milimetros += 12;
+      break;
+    case '44st150':
+      preu += calculaDesconte(76.16);
+      milimetros += 8;
+      break;
+    case '66st150':
+      preu += calculaDesconte(97.14);
+      milimetros += 12;
+      break;
+    case '44st167':
+      preu += calculaDesconte(76.16);
+      milimetros += 8;
+      break;
+    case '66st167':
+      preu += calculaDesconte(97.14);
+      milimetros += 12;
+      break;
+    case 'ma4':
+      preu += calculaDesconte(17);
+      milimetros += 4;
+      break;
+    case 'ma5':
+      preu += calculaDesconte(19.59);
+      milimetros += 5;
+      break;
+    case 'novo4':
+      preu += calculaDesconte(34.71);
+      milimetros += 4;
+      break;
+    case 'novo5':
+      preu += calculaDesconte(42.94);
+      milimetros += 5;
+      break;
+    case 'clxt4':
+      preu += calculaDesconte(53.21);
+      milimetros += 4;
+      break;
+    case 'clxt6':
+      preu += calculaDesconte(66.4);
+      milimetros += 6;
+      break;
+    case 'clstb6':
+      preu += calculaDesconte(61.03);
+      milimetros += 6;
+      break;
+    case 'clst4':
+      preu += calculaDesconte(50.9);
+      milimetros += 4;
+      break;
+    case 'clst6':
+      preu += calculaDesconte(53.94);
+      milimetros += 6;
+      break;
+    case 'clst1674':
+      preu += calculaDesconte(50.9);
+      milimetros += 4;
+      break;
+    case 'cslt1676':
+      preu += calculaDesconte(53.94);
+      milimetros += 6;
+      break;
+    case 'sun4':
+      preu += calculaDesconte(43.82);
+      milimetros += 4;
+      break;
+    case 'sun6':
+      preu += calculaDesconte(56.64);
+      milimetros += 6;
+      break;
+    case 'ref6':
+      preu += calculaDesconte(34.16);
+      milimetros += 6;
+      break;
+    case 'clst6150':
+      preu += calculaDesconte(73.49);
+      milimetros += 6;
+      break;
+    case 'clst6120':
+      preu += calculaDesconte(79.62);
+      milimetros += 6;
+      break;
+    case 'stop5':
+      preu += calculaDesconte(35.29);
+      milimetros += 5;
+      break;
+    case 'plan4':
+      preu += calculaDesconte(22.36);
+      milimetros += 4;
+      break;
+    case 'plan6':
+      preu += calculaDesconte(32.14);
+      milimetros += 6;
+      break;
+    case 'planfn4':
+      preu += calculaDesconte(11.9);
+      milimetros += 4;
+      break;
+    case 'planfn6':
+      preu += calculaDesconte(18.7);
+      milimetros += 6;
+      break;
+    case 'kglas':
+      preu += calculaDesconte(20.6);
+      milimetros += 6;
+      break;
+    }
   }
-  //aqui hauría de fer que comprovas si no han triat cap preu al formulari
-  document.getElementById('error').innerHTML = '<p>Has de triar algun tipus de vidre!</p>';
-  return 0;
+
+}
+//aqui hauría de fer que comprovas si no han triat cap preu al formulari
+document.getElementById('error').innerHTML = '<p>Has de triar algun tipus de vidre!</p>';
+return 0;
 }
 
 function calculaDesconte(preu) {
